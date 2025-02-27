ec8f0129bbef788d0952c559de26645ab918db53
Amit <yga995@gmail.com>
2025-02-27 15:10:37 +0530

    omplete

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,11 @@ import userRoute from "./route/user.route.js";
 
 const app = express();
 
-app.use(cors({
+app.use(cors,{
     origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-  }));
+  });
 
 app.use(express.json());
 
