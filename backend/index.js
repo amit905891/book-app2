dff734ac0c2098e84339f1f3b3f12aba9f6ee9dc
Amit <yga995@gmail.com>
2025-02-27 15:08:09 +0530

    ompleted

--- backend/index.js
+++ backend/index.js
@@ -8,18 +8,24 @@ import userRoute from "./route/user.route.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }));
 
 app.use(express.json());
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
-const URI = process.env.MongoDBURI;
+const MongoDBURI = process.env.MongoDBURI;
+console.log(URI);
+
 
 // connect to mongoDB
 try {
-    mongoose.connect(URI);
+    mongoose.connect(MongoDBURI);
     console.log("Connected to mongoDB");
 } catch (error) {
     console.log("Error: ", error);
