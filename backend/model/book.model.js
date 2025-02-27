ea1392237f718f4e217712d4e66d28427c0ad6eb
Amit <yga995@gmail.com>
2025-02-27 16:09:35 +0530

    omp

--- backend/model/book.model.js
+++ backend/model/book.model.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 const bookSchema = mongoose.Schema({
-    name: String,
-    price: Number,
-    category: String,
-    image: String,
-    title: String,
+    books: {
+        name: String,
+        price: Number,
+        category: String,
+        image: String,
+        title: String
+    }
 });
 const Book = mongoose.model("Book", bookSchema);
 
